c4dd0894aebf37aca841a8dfc88289a0896a006c
Chiyle Briggins <chiylebriggins@MacBook-Pro-627.local>
2025-11-13 15:00:12 -0500

    PA02 Part 2: Added variables and operators in JavaScript

---
+++ app.js
@@ -0,0 +1,41 @@
+console.log("✅ JS is linked and running!");
+
+
+// String
+let name = "Chiyle";
+
+// Number
+let age = 22;
+
+// Boolean
+let isStudent = true;
+
+// Array
+let favoriteFoods = ["Fried Rice", "Burger", "Chicken"];
+
+// Object
+let student = {
+  name: "Chiyle",
+  major: "Computer Science",
+  year: "Senior"
+};
+
+// Null
+let currentJob = null;
+
+// Log them to the console
+console.log("Name:", name);
+console.log("Age:", age);
+console.log("Is a student?", isStudent);
+console.log("Favorite Foods:", favoriteFoods);
+console.log("Student Info:", student);
+console.log("Current Job:", currentJob);
+
+// Operators
+let sum = age + 5;
+let isAdult = age >= 18;
+let canGraduate = isStudent && (student.year === "Senior");
+
+console.log("Sum (age + 5):", sum);
+console.log("Is an adult?", isAdult);
+console.log("Can graduate?", canGraduate);
